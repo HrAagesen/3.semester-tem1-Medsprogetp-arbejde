67ba1726c911fa07383d0c737e0db1cd17309fc8
HrAagesen <33120123+HrAagesen@users.noreply.github.com>
2018-09-11 00:31:56 +0200

    Add files via upload
    fix, bruger info begyndt

--- js/js.js
+++ js/js.js
@@ -1,5 +1,7 @@
 // $( document ).ready(function() {
 
+
+//dagbog klik og vis mere
   function dag1() {
 var x = document.getElementById("dag1");
 if (x.style.display !== "block") {
@@ -53,5 +55,62 @@ if (x.style.display !== "block") {
     x.style.display = "none";
 }
 }
+//dagbog slut
 
+//øvelser klik og vis
+function opg1() {
+var x = document.getElementById("opg1");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function opg2() {
+var x = document.getElementById("opg2");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function opg3() {
+var x = document.getElementById("opg3");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function opg4() {
+var x = document.getElementById("opg4");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function opg5() {
+var x = document.getElementById("opg5");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function opg6() {
+var x = document.getElementById("opg6");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+//øvelser slut
 // )};
