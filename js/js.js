f7381c0f35afa8a554b2503f09779e3d136a52a1
HrAagesen <mikkel_aagesen@hotmail.com>
2018-09-10 22:44:35 +0200

    dagbog

---
+++ js/js.js
@@ -0,0 +1,57 @@
+// $( document ).ready(function() {
+
+  function dag1() {
+var x = document.getElementById("dag1");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function dag2() {
+var x = document.getElementById("dag2");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function dag3() {
+var x = document.getElementById("dag3");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function dag4() {
+var x = document.getElementById("dag4");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function dag5() {
+var x = document.getElementById("dag5");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function dag6() {
+var x = document.getElementById("dag6");
+if (x.style.display !== "block") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+// )};
